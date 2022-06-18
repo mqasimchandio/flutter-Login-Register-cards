4de048e405ec0ead5db14ab10293864d16175ee5
mqasimchandio <102449031+mqasimchandio@users.noreply.github.com>
2022-06-18 17:54:41 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '/screens/chat_screen.dart';
+import '/screens/welcome_screen.dart';
+import '/screens/login_screen.dart';
+import '/screens/registration_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(
+            color: Colors.black54,
+          ),
+        ),
+      ),
+
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+      home: ChatScreen(),
+    );
+  }
+}
